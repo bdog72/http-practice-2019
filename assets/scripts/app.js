5c713d8c8e398735e6e38b6d9b9cf92f66f47660
Brian Bycynski <high5bri@hotmail.com>
2019-11-17 04:22:39 -0500

    Initial Commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,21 +5,59 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+function sendHttpRequest(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
 
-xhr.responseType = "json";
+    xhr.responseType = "json";
 
-xhr.onload = function() {
-  // const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response;
+    xhr.onload = function() {
+      resolve(xhr.response);
+      // const listOfPosts = JSON.parse(xhr.response);
+    };
+
+    xhr.send(JSON.stringify(data));
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  const listOfPosts = responseData;
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
     listElement.append(postEl);
   }
-};
+}
 
-xhr.send();
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId
+  };
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
+fetchButton.addEventListener("click", fetchPosts);
+// fetchPosts();
+// createPost("DUMMY", "A dummy post!");
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+
+  createPost(enteredTitle, enteredContent);
+});
